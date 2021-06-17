7581faefb2aa1ac7d71cc86a490d1167b962efb5
Sumit2790 <86053674+Sumit2790@users.noreply.github.com>
2021-06-17 18:19:04 +0530

    My C++ program
    My C++ program

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string a, b, c;//Variable Declaration
+a= "Shital \n";//Assigning value
+
+b ="Sumit \n";
+c="World ";
+cout<< a ;
+cout<< b;
+cout<< c;
+return 0;
+}
