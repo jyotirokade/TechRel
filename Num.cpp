055ceabcee397cdd27a1efeb45dab76ee58fe401
Sumit2790 <86053674+Sumit2790@users.noreply.github.com>
2021-06-17 18:32:49 +0530

    Program for addition of numbers

---
+++ Num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string a, b, c;//Variable Declaration
+a= "Shital \n";//Assigning value
+
+b ="Sumit \n";
+c="World ";
+cout<< a ;
+cout<< b;
+cout<< c;
+int x=10;
+cout<<x+20;
+return 0;
+}
